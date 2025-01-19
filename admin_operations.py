d2d63ce0167a18d3f12287ee6efac59c7050766e
David Gaspar <david.gaspar97@yahoo.com>
2025-01-19 20:04:33 +0200

    added commentary where change needs to happen

--- admin_operations.py
+++ admin_operations.py
@@ -3,6 +3,8 @@ import json
 
 def remove_user(user_to_be_deleted: str, bank_path: str = "bank.json", auth_path: str = "auth.json" , clients_path: str = "clients.json"):
 
+
+    # here I need to change to make this more efficient
     with open(bank_path, "r") as f:
         accounts = json.loads(f.read())
 
